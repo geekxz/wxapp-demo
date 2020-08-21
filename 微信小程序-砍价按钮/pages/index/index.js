5e85536c0782f8bc7308a08ea8b099d9cf279f91
陈东东 <13298176895@163.com>
2020-08-21 21:19:26 +0800

    添加砍价特效按钮

---
+++ 微信小程序-砍价按钮/pages/index/index.js
@@ -0,0 +1,98 @@
+/**
+ * Created by 陈东东
+ * Author: 陈东东	<13298176895@163.com>
+ * 微信公号: 极客小寨工作室
+ * Date: 2020/4/15
+ * Time: 19:40
+ */
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    animationData: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.doAnimation();
+  },
+  // 动画部分
+  doAnimation: function(){
+    var animation = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease',
+    })
+    this.animation = animation
+    var next = true;
+    //连续动画关键步骤
+    setInterval(function () {
+      if (next) {
+        this.animation.scale(0.95).step()
+        next = !next;
+      } else {
+        this.animation.scale(1).step()
+        next = !next;
+      }
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 500)
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
